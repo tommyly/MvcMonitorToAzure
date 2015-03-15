bd0e897ab491ab0832ceb4f752c82117e50b98f6
Tommy Ly <tommyly764@gmail.com>
2015-03-15 19:06:19 +0000

    first

---
+++ MVCToAzure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCToAzure.Startup))]
+namespace MVCToAzure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
